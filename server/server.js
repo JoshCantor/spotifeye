5b08fd7f0ca0fc5ca3baae028d5debf79046df08
JoshCantor <josh.s.cantor@gmail.com>
2016-02-18 21:28:34 -0800

    created server, added knex, pg, locus, morgan, body-parser

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	morgan = require('morgan'),
+	knex = require('../db/knex');
+
+require('locus');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(morgan('tiny'));
+
+
+app.listen(3000, function() {
+	console.log('listening on 3000...');
+});
