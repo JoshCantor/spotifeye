0c6cc3c743aa64bb4e21ef7b1040b976441f8262
JoshCantor <josh.s.cantor@gmail.com>
2016-02-18 22:36:45 -0800

    installed express; added index.html to test; added route to get test spotify api

--- server/server.js
+++ server/server.js
@@ -2,13 +2,32 @@ var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
 	morgan = require('morgan'),
-	knex = require('../db/knex');
-	
+	knex = require('../db/knex'),
+	request = require('request');
+
 require('dotenv').config();
 require('locus');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan('tiny'));
+
+app.get("/", function(req, res, next) {
+	res.render("index.html");
+});
+
+app.get("/song", function(req, res, next) {
+	request.get("https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy", function(error, response, body) {
+	        if (error) {
+	            res.status(500).send("You got an error - " + error);
+	        } else if (!error && response.statCode >= 300) {
+	            res.status(500).send("Something went wrong! Status: " + response.statusCode);
+	        }
+	        if (!error && response.statusCode === 200) {
+	            var data = JSON.parse(body);
+	            console.log(data);
+	        }
+	    });
+});
 
 
 app.listen(3000, function() {
