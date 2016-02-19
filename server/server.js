b4afc63442e034935d8c19ccb49c365c8aab7ed3
JoshCantor <josh.s.cantor@gmail.com>
2016-02-18 23:05:12 -0800

    base route test: gets json from spotify api and successfuly stores it in database

--- server/server.js
+++ server/server.js
@@ -13,22 +13,21 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan('tiny'));
 
 app.get("/", function(req, res, next) {
-	res.render("/views/index");
+	request.get("https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy", function(error, response, body) {
+        if (error) {
+            res.status(500).send("You got an error - " + error);
+        } else if (!error && response.statCode >= 300) {
+            res.status(500).send("Something went wrong! Status: " + response.statusCode);
+        }
+        if (!error && response.statusCode === 200) {
+            var data = JSON.parse(body);
+            knex('songs').insert({json_data: data}).returning('json_data')
+            .then(function(json) {
+            	console.log('json', json);
+            });
+        }
+    });
 });
-
-// app.get("/song", function(req, res, next) {
-// 	request.get("https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy", function(error, response, body) {
-// 	        if (error) {
-// 	            res.status(500).send("You got an error - " + error);
-// 	        } else if (!error && response.statCode >= 300) {
-// 	            res.status(500).send("Something went wrong! Status: " + response.statusCode);
-// 	        }
-// 	        if (!error && response.statusCode === 200) {
-// 	            var data = JSON.parse(body);
-// 	            console.log(data);
-// 	        }
-// 	    });
-// });
 
 
 app.listen(3000, function() {
