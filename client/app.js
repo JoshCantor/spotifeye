9a7702e0e8a9819008b05c0e64962ba3060792c1
JoshCantor <josh.s.cantor@gmail.com>
2016-02-19 17:05:34 -0800

    connected angular app to express server

---
+++ client/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('spotifeye', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: "/client/d3template.html",
+		controller: "D3"
+	});
+});
+
+app.controller('D3', function($scope) {
+	// $scope.test = 'test';
+});
+
+// app.directive('d3', function() {
+// 	return {
+// 		templateUrl: 'd3template.html',
+// 		link: function(scope, element, attributes){
+// 		}
+// 	};
+// });
+
+
--- server/server.js
+++ server/server.js
@@ -8,32 +8,35 @@ var express = require('express'),
 // require('dotenv').config();
 require('locus');
 
-app.set('view engine', 'ejs');
-
-app.use(express.static('/views'));
+app.use('/client', express.static(__dirname + '/../client'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan('tiny'));
 
 app.get("/", function(req, res, next) {
-	request.get("https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy", function(error, response, body) {
-        if (error) {
-            res.status(500).send("You got an error - " + error);
-        } else if (!error && response.statCode >= 300) {
-            res.status(500).send("Something went wrong! Status: " + response.statusCode);
-        }
-        if (!error && response.statusCode === 200) {
-            var data = JSON.parse(body);
-            knex('songs').insert({json_data: data}).returning('json_data')
-            .then(function(json) {
-            	console.log('json', json);
-            });
-        }
-    });
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/dashboard', function(req, res, next) {
-    res.render(__dirname + '/views/index');
-});
+// app.get("/", function(req, res, next) {
+// 	request.get("https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy", function(error, response, body) {
+//         if (error) {
+//             res.status(500).send("You got an error - " + error);
+//         } else if (!error && response.statCode >= 300) {
+//             res.status(500).send("Something went wrong! Status: " + response.statusCode);
+//         }
+//         if (!error && response.statusCode === 200) {
+//             var data = JSON.parse(body);
+//             knex('songs').insert({json_data: data}).returning('json_data')
+//             .then(function(json) {
+//             	console.log('json', json);
+//             });
+//         }
+//     });
+// });
+
+// app.get('/dashboard', function(req, res, next) {
+//     knex('songs').then(function(albumsData){
+//     });
+// });
 
 
 app.listen(3000, function() {
