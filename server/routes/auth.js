5b4c7fdb03883c45696f5c37a6ab22de96daf852
Shashik Banda <shashik.banda@gmail.com>
2016-02-21 20:12:27 -0800

    re-created authentication for spotify. successfully can obtain access_token for authorized user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,40 +1,36 @@
 var express = require("express");
 var router = express.Router();
+var url = require('url');
+var request = require('request');
 
 var passport = require("passport");
 var SpotifyStrategy = require('passport-spotify').Strategy;
 
-passport.use(new SpotifyStrategy({
-	clientID:"1857c7c8664f4600b762dc603227be89",
-	clientSecret:"00d15fc31efb436ea0a012ef7af2a248",
-	callbackURL:"http://localhost:3000/auth/spotify/callback"
-},
-function(accessToken, refreshToken, profile, done){
+var client_id = "1857c7c8664f4600b762dc603227be89";
+var client_secret = "00d15fc31efb436ea0a012ef7af2a248";
+var redirect_uri = "http://localhost:3000/auth/spotify/callback";
 
-	return done(null, profile);
-}))
+router.get('/spotify', function(req, res){
+    var scope = 'user-read-private user-read-email';
+    res.redirect('https://accounts.spotify.com/authorize?client_id='+client_id+'&client_secret='+client_secret+'&redirect_uri='+redirect_uri+'&scope='+scope+'&response_type=code'+'&show_dialog=true')
+})
 
-passport.serializeUser(function(user, done) {
-	done(null, user);
-});
-passport.deserializeUser(function(obj, done) {
-	done(null, obj);
-});
-
-router.get('/spotify', passport.authenticate('spotify'), function(req, res){
-    // The request will be redirected to spotify for authentication, so this
-    // function will not be called.
-    console.log("spotify is called to authenticate")
-});
-
-router.get('/spotify/callback', passport.authenticate('spotify', { failureRedirect: '/login' }), function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/success');
-});
-
-router.get('/success',function(req,res){
-	console.log("sucessful auth")
-});
+router.get('/spotify/callback', function(req,res){
+    var code = req.query.code;
+    request.post({
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+            code:code,
+            redirect_uri: 'http://localhost:3000/auth/spotify/callback',
+            grant_type: 'authorization_code'
+        },
+        headers: {
+            'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+        }
+    }, function(error, response, body){
+        res.send(body)
+    })
+})
 
 module.exports = {
 	router:router,
