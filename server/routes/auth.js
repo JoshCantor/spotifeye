fc1bd9924f84cdc5eab376c3e557f82032ef5f11
Shashik Banda <shashik.banda@gmail.com>
2016-02-22 17:09:35 -0800

    refactored code slightly to align better.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -40,33 +40,31 @@ router.get('/spotify/callback', function(req,res){
 
             knex('users').where({user_id:user_id}).then(function(data){
                 if(data.length === 0){
-                    knex('users').insert({display_name:display_name,
-                                         user_id:user_id,
-                                         profile_pic:profile_pic
-                                        }).then(function(test){ // INSERTS SONGS IF FIRST TIME LOGINING IN
-                                            request.get('https://api.spotify.com/v1/me/tracks?access_token='+access_token, function(error, response,body){
-                                                var responseJSON = JSON.parse(body);
-                                                for(var i = 0; i < responseJSON.items.length; i++){
-                                                    var track_id = responseJSON.items[i].track.id;
-                                                    var track_name = responseJSON.items[i].track.name;
-                                                    var track_popularity = responseJSON.items[i].track.popularity;
-                                                    var track_art = responseJSON.items[i].track.album.images[0].url;
-                                                    var preview_url = responseJSON.items[i].track.preview_url;
-                                                    function generateInsertCB(track_id,track_name,track_popularity,track_art, preview_url){
-                                                        knex('tracks').where({track_id:track_id}).then(function(data){
-                                                            if(data.length === 0){
-                                                                knex('tracks').insert({track_id:track_id,
-                                                                                       track_name:track_name,
-                                                                                       track_popularity:track_popularity,
-                                                                                       album_art:track_art,
-                                                                                       preview_url:preview_url}).then(function(){})
-                                                            }
-                                                        })
-                                                    }
-                                                    knex('tracks').where({track_id:track_id}).then(generateInsertCB(track_id,track_name,track_popularity,track_art, preview_url))
-                                                }
-                                            })
-                                        })
+                    knex('users').insert({display_name:display_name,user_id:user_id,profile_pic:profile_pic})
+                    .then(function(test){ // INSERTS SONGS IF FIRST TIME LOGINING IN
+                        request.get('https://api.spotify.com/v1/me/tracks?access_token='+access_token, function(error, response,body){
+                            var responseJSON = JSON.parse(body);
+                            for(var i = 0; i < responseJSON.items.length; i++){
+                                var track_id = responseJSON.items[i].track.id;
+                                var track_name = responseJSON.items[i].track.name;
+                                var track_popularity = responseJSON.items[i].track.popularity;
+                                var track_art = responseJSON.items[i].track.album.images[0].url;
+                                var preview_url = responseJSON.items[i].track.preview_url;
+                                function generateInsertCB(track_id,track_name,track_popularity,track_art, preview_url){
+                                    knex('tracks').where({track_id:track_id}).then(function(data){
+                                        if(data.length === 0){
+                                            knex('tracks').insert({track_id:track_id,
+                                                                   track_name:track_name,
+                                                                   track_popularity:track_popularity,
+                                                                   album_art:track_art,
+                                                                   preview_url:preview_url}).then(function(){})
+                                        }
+                                    })
+                                }
+                                knex('tracks').where({track_id:track_id}).then(generateInsertCB(track_id,track_name,track_popularity,track_art, preview_url))
+                            }
+                        })
+                    })
                 }
                 else{
                     request.get('https://api.spotify.com/v1/me/tracks?access_token='+access_token, function(error, response,body){
