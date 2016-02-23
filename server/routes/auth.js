8909e4cea29dfcd9f715a8c4fe6cb14f9975e9e8
Shashik Banda <shashik.banda@gmail.com>
2016-02-22 16:38:22 -0800

    can successfully add data from authenticated users stared song list.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,7 +10,7 @@ var client_secret = "00d15fc31efb436ea0a012ef7af2a248";
 var redirect_uri = "http://localhost:3000/auth/spotify/callback";
 
 router.get('/spotify', function(req, res){
-    var scope = 'user-read-private user-read-email playlist-read-private streaming';
+    var scope = 'user-read-private user-read-email playlist-read-private streaming user-library-read';
     res.redirect('https://accounts.spotify.com/authorize?client_id='+client_id+'&client_secret='+client_secret+'&redirect_uri='+redirect_uri+'&scope='+scope+'&response_type=code'+'&show_dialog=true')
 })
 
@@ -38,19 +38,53 @@ router.get('/spotify/callback', function(req,res){
             var display_name = userInfoJSON.display_name;
             var profile_pic = userInfoJSON.images[0].url;
 
-            // knex('users').where({user_id:user_id}).then(function(data){
-            //     if(data.length === 0){
-            //         console.log("first and only time adding this user")
-            //         knex('users').insert({display_name:display_name,
-            //                              user_id:user_id,
-            //                              profile_pic:profile_pic
-            //                             }).then(function(test){
-            //                                 request.get('https://api.spotify.com/v1/users/'+user_id+'/playlists?access_token='+access_token, function(error, response, body){
-            //                                     var playlistJSON = JSON.parse(body);
-            //                                 })
-            //                             })
-            //     }
-            // })
+            knex('users').where({user_id:user_id}).then(function(data){
+                if(data.length === 0){
+                    knex('users').insert({display_name:display_name,
+                                         user_id:user_id,
+                                         profile_pic:profile_pic
+                                        }).then(function(test){
+                                            request.get('https://api.spotify.com/v1/me/tracks?access_token='+access_token, function(error, response,body){
+                                                var responseJSON = JSON.parse(body);
+                                                for(var i = 0; i < responseJSON.items.length; i++){
+                                                    var track_id = responseJSON.items[i].track.id;
+                                                    var track_name = responseJSON.items[i].track.name;
+                                                    var track_popularity = responseJSON.items[i].track.popularity;
+                                                    var track_art = responseJSON.items[i].track.album.images[0].url;
+                                                    var preview_url = responseJSON.items[i].track.preview_url;
+                                                    function generateInsertCB(track_id,track_name,track_popularity,track_art, preview_url){
+                                                        knex('tracks').insert({track_id:track_id,
+                                                                               track_name:track_name,
+                                                                               track_popularity:track_popularity,
+                                                                               album_art:track_art,
+                                                                               preview_url:preview_url}).then(function(){})
+                                                    }
+                                                    knex('tracks').where({track_id:track_id}).then(generateInsertCB(track_id,track_name,track_popularity,track_art, preview_url))
+                                                }
+                                            })
+                                        })
+                }
+                else{
+                    request.get('https://api.spotify.com/v1/me/tracks?access_token='+access_token, function(error, response,body){
+                        var responseJSON = JSON.parse(body);
+                        for(var i = 0; i < responseJSON.items.length; i++){
+                            var track_id = responseJSON.items[i].track.id;
+                            var track_name = responseJSON.items[i].track.name;
+                            var track_popularity = responseJSON.items[i].track.popularity;
+                            var track_art = responseJSON.items[i].track.album.images[0].url;
+                            var preview_url = responseJSON.items[i].track.preview_url;
+                            function generateInsertCB(track_id,track_name,track_popularity,track_art, preview_url){
+                                knex('tracks').insert({track_id:track_id,
+                                                       track_name:track_name,
+                                                       track_popularity:track_popularity,
+                                                       album_art:track_art,
+                                                       preview_url:preview_url}).then(function(){})
+                            }
+                            knex('tracks').where({track_id:track_id}).then(generateInsertCB(track_id,track_name,track_popularity,track_art, preview_url))
+                        }
+                    })
+                }
+            })
         })
     })
 })
