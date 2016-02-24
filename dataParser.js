76f81acdaed0b604b5e0d1420fd4b211d978d309
JoshCantor <josh.s.cantor@gmail.com>
2016-02-24 10:28:59 -0800

    removed bad echonest data; started parsing good data

---
+++ dataParser.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+var parsed = {}
+	;
+
+fs.readdir('./jsondata', function(err, files) {
+	if (err) throw err;
+	files.forEach(function(file) {
+		fs.readFile("./jsondata/" + file, function(err, data) {
+			if (err) throw err;
+			var json = JSON.parse(data.toString());
+			if (json.response.track !== undefined) {
+				console.log(file, 'dance', json.response.track.audio_summary.danceability);
+			}		
+		});
+	});
+});
+
