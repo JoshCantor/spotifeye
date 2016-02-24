0186bc6e12c82ff244a0d52a09cf5bf9d4fd7ab2
Mauricio Andrades <mod.and.op@gmail.com>
2016-02-24 11:48:20 -0800

    added larger test array, and required into echonest.js

--- echonest/echonest.js
+++ echonest/echonest.js
@@ -1,15 +1,16 @@
 /** @type {srcipt} [calls echonest api to circumvent the rate limiting] */
 var request = require('request');
+var trackids = require('./arr');
 var dotenv = require('dotenv').config();
 var apiKey = process.env.echonest_key;
 
 console.log(apiKey);
-arr = ["03kND9B8jZBbFlEtUZX7FC"];
+// trackids = ["03kND9B8jZBbFlEtUZX7FC"];
 
-for (var i = 0; i < arr.length; i++) {
+for (var i = 0; i < trackids.length; i++) {
 
     var options = {
-        url: "http://developer.echonest.com/api/v4/track/profile?api_key="+ apiKey + "&format=json&id=spotify:track:" + arr[i] + "&bucket=audio_summary"
+        url: "http://developer.echonest.com/api/v4/track/profile?api_key="+ apiKey + "&format=json&id=spotify:track:" + trackids[i] + "&bucket=audio_summary"
     };
 
     console.log(options.url);
@@ -23,4 +24,3 @@ for (var i = 0; i < arr.length; i++) {
 
     request(options, callback);
 }
-
