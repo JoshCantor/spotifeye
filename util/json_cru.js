0ac83302c9d1f43ca68c4d348f395b81f672c20f
Jon Tejada <jon@jontejada.com>
2016-02-24 15:07:15 -0800

    working json writing

--- util/json_cru.js
+++ util/json_cru.js
@@ -1,7 +1,8 @@
 var knex = require('../server/db/knex');
 var fs = require('fs');
 var request = require('request');
-var dotenv = require('dotenv').config();
+require('dotenv').load();
+            // console.log('key', process.env.echonest_key);
 
 var tracksToCheck = {};
 var tracks = [];
@@ -10,7 +11,7 @@ knex.column('track_id').select().from('tracks').then(function(data) {
     for (var i = 0; i < data.length; i++) {
         tracksToCheck[data[i].track_id] = true;
     }
-    fs.readdir('../jsondata', function(err, files) {
+    fs.readdir('./jsondata', function(err, files) {
         if (err) throw err;
         files.forEach(function(file) {
         	var fileName = file.split('.')[0];
@@ -37,11 +38,28 @@ knex.column('track_id').select().from('tracks').then(function(data) {
         console.log('skipped track count = ',count);
         ////////////////////////////////////////////////////
         //pass tracks array into EchoNest API call code here:
-        // for (var j = 0; j<20; j++) {
-        //     request("http://developer.echonest.com/api/v4/track/profile?api_key=" + process.env.echonest_key + "&format=json&id=spotify:track:" + tracks[j] + "&bucket=audio_summary")
-        //         .pipe(
-        //             fs.createWriteStream(tracks[j]));
-        // }
+        for (var j = 0; j<20; j++) {
+            // console.log(tracks[j]);
+            var options = {
+                url: "http://developer.echonest.com/api/v4/track/profile?api_key=" + process.env.echonest_key + "&format=json&id=spotify:track:" + tracks[j] + "&bucket=audio_summary"
+            };
+            request(options, generateFileCallback(j));
+        }
     });
 });
+
+function generateFileCallback(j) {
+    return function callback(error, response, body) {
+        // console.log(body);
+        if (!error && response.statusCode === 200) {
+            var info = JSON.parse(body);
+            // console.log(info);
+            fs.writeFile('jsondata/' + tracks[j] + '.json', body);
+        } else {
+            console.log('no work');
+        }
+    };
+}
+
+
 
