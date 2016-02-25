54982fd81fba2738f33b78b139dc220952f9d127
JoshCantor <josh.s.cantor@gmail.com>
2016-02-24 16:04:22 -0800

    removed factories

--- client/public/scripts/controllers/bubblescontroller.js
+++ client/public/scripts/controllers/bubblescontroller.js
@@ -4,13 +4,4 @@ angular.module('spotifeyeApp')
 	.controller('BubbleController', function($scope, $http, $location, BubbleData) {
 
 		$scope.bubbleData = BubbleData.data;
-		console.log($scope.bubbleData);
-		// $scope.data = [];
-
-		// $scope.getData = function() {
-		// 	$http.get('/user/bubble').then(function(data) {
-		// 		$scope.data = data;
-		// 	});
-		// };
-		// $scope.getData();
 	});
