e101a22404b838f593d63127497bfce75cf7295b
Mauricio Andrades <mod.and.op@gmail.com>
2016-02-24 21:29:29 -0800

    bound util folder to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      files: ['Gruntfile.js', 'server/**/*.js', 'client/**/*.js'],
+      files: ['Gruntfile.js', 'dataParser.js' ,'server/**/*.js', 'client/**/*.js', 'util/**/*.js'],
       options: {
         reporter: require('jshint-stylish'),
         globals: {
