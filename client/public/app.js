336eb5e86d54ba4d1f3d1b0203ac61945a86aeeb
Jon Tejada <jon@jontejada.com>
2016-02-25 14:44:17 -0800

    working time graph

--- client/public/app.js
+++ client/public/app.js
@@ -27,7 +27,12 @@ angular.module('spotifeyeApp')
         })
         .when('/dashboard/edgeBundle', {
             templateUrl: 'views/edgeTemplate.html',
-            controller: 'EdgeController'
+            controller: 'TimeController'
+            // resolve: {
+            //     TimeData: function($http){
+            //         return $http.get('user/dashboard/time');
+            //     }
+            // }
         })
         .when('/dashboard/chords', {
             templateUrl: 'views/chordTemplate.html',
